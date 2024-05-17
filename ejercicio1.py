f70625ddd8a861f99965de7bd9283a10dcc2f15b
Ana Laura Lara Chairez <analauris2103@gmail.com>
2024-05-16 20:20:44 -0600

    PRUEBA

---
+++ ejercicio1.py
@@ -0,0 +1,9 @@
+# Calculando x1
+x1 = (-4 + (4**2 - 4*(2 * (-6)))**0.5) / (2*2)
+
+# Calculando x2
+x2 = (-4 - (4**2 - 4*(2 * (-6)))**0.5) / (2*2)
+
+# Imprimiendo los resultados
+print(x1)  # Resultado con el signo más
+print(x2)  # Resultado con el signo menos
